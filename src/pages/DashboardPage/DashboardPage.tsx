1ad8de6622e52eec05676b8521ede242118fc908
fomenko.german <fomenko.german@wb.ru>
2026-06-30 10:03:12 +0500

    Add verse image dashboard block

--- src/pages/DashboardPage/DashboardPage.tsx
+++ src/pages/DashboardPage/DashboardPage.tsx
@@ -1,8 +1,10 @@
+import { CalendarCard } from '../../widgets/CalendarCard/CalendarCard';
 import { Header } from '../../widgets/Header/Header';
 import { MemorizationSection } from '../../widgets/MemorizationSection/MemorizationSection';
 import { PracticeCardsGrid } from '../../widgets/PracticeCardsGrid/PracticeCardsGrid';
 import { QuoteCard } from '../../widgets/QuoteCard/QuoteCard';
 import { Sidebar } from '../../widgets/Sidebar/Sidebar';
+import { VerseImageBlock } from '../../widgets/VerseImageBlock/VerseImageBlock';
 import styles from './DashboardPage.module.css';
 
 export function DashboardPage() {
@@ -11,12 +13,16 @@ export function DashboardPage() {
       <Sidebar />
       <main className={styles.dashboard}>
         <Header />
-        <MemorizationSection />
+        {/* <MemorizationSection /> */}
         <div className={styles.contentGrid}>
           <section className={styles.mainCol}>
+            <VerseImageBlock />
             <PracticeCardsGrid />
             <QuoteCard />
           </section>
+          <aside className={styles.sideCol}>
+            <CalendarCard />
+          </aside>
         </div>
       </main>
     </div>
